b9fb49e67e95128e1f426dc07c8c6a35b1aed1b3
Lukáš Unger <looky.msc@gmail.com>
2018-06-14 15:04:53 +0200

    Added Nette and NEON patchers

--- build/scoper.inc.php
+++ build/scoper.inc.php
@@ -10,6 +10,18 @@ return [
 			}
 			return str_replace('__DIR__ . \'/..', '\'phar://phpstan.phar', $content);
 		},
+		function (string $filePath, string $prefix, string $content): string {
+			if ($filePath !== 'vendor/nette/di/src/DI/Compiler.php') {
+				return $content;
+			}
+			return str_replace('|Nette\\\\DI\\\\Statement', sprintf('|\\\\%s\\\\Nette\\\\DI\\\\Statement', $prefix), $content);
+		},
+		function (string $filePath, string $prefix, string $content): string {
+			if ($filePath !== 'conf/config.neon') {
+				return $content;
+			}
+			return str_replace('PhpParser\\', sprintf('%s\\PhpParser\\', $prefix), $content);
+		}
 	],
 	'whitelist' => [
 		'PHPStan\*',
